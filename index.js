5d1caf2bea5ec3b9f9739332a2e227130b3d99bc
phamtienk4 <25a4041924@hvnh.edu.vn>
2024-12-18 21:26:54 +0700

    asdfs

--- index.js
+++ index.js
@@ -6,11 +6,6 @@ const middlewares = jsonServer.defaults();
 server.use(middlewares);
 server.use(jsonServer.bodyParser);  // Đảm bảo json-server có thể phân tích JSON
 
-// POST endpoint xử lý yêu cầu gửi giỏ hàng
-server.post('/cart', (req, res) => {
-    console.log('Data received:', req.body);  // Kiểm tra xem dữ liệu có đúng không
-    res.status(200).json(req.body);  // Trả lại dữ liệu nhận được
-});
 
 const port = process.env.PORT || 8080;
 server.use(router);
