1e7b4daee9bb28cf9118c594af9e5a0bde5ce7d8
phamtienk4 <25a4041924@hvnh.edu.vn>
2024-12-21 16:00:12 +0700

    ok

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const middlewares = jsonServer.defaults();
 server.use(middlewares);
 server.use(jsonServer.bodyParser);  // Đảm bảo json-server có thể phân tích JSON
 
-
 const port = process.env.PORT || 8080;
 server.use(router);
 server.listen(port, () => {
